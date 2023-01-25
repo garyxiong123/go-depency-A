b48c4de58accbb02495791a785b9c9f04e51ae6c
garyxiong123 <502238410@qq.com>
2023-01-25 17:46:51 +0800

    init

--- b.go
+++ b.go
@@ -1,8 +1,8 @@
 package B
 
-func Go_dependency_A_V1() {
-	println("go_dependency_B_V1")
-}
+//func Go_dependency_A_V1() {
+//	println("go_dependency_B_V1")
+//}
 
 func Go_dependency_A_V2() {
 	println("go_dependency_B_V1")

86a8d8f3effcca82e6cb98c3f855ec03ec0a327d
garyxiong123 <502238410@qq.com>
2023-01-25 18:54:13 +0800

    init

--- A1/b.go
+++ A1/b.go
@@ -1,4 +1,4 @@
-package B
+package A1
 
 func Go_dependency_A_V1() {
 	println("go_dependency_B_V1")
--- A2/b.go
+++ A2/b.go
@@ -1,4 +1,4 @@
-package B
+package A2
 
 func Go_dependency_A_V2() {
 	println("Go_dependency_A_V2")
